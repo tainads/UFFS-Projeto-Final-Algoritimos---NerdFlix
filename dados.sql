16534a7191d812650cff65f6f16b9b9f013edcb6
Tainá Dalla Santa <105253897+tainads@users.noreply.github.com>
2022-08-12 00:15:35 -0300

    Arquivos de dados
    Script para popular o banco inicial

---
+++ dados.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO produtos (id, nome, tipo, preco, disponivel) VALUES (1, 'The Big Bang Theory', '1', '20,50', 'SIM');
+INSERT INTO produtos (id, nome, tipo, preco, disponivel) VALUES (2, 'Suits', '1', '20,50', 'SIM');
+INSERT INTO produtos (id, nome, tipo, preco, disponivel) VALUES (3, 'Eternal Sunshine of the Spotless Mind', '2', '10,00', 'SIM');
+INSERT INTO produtos (id, nome, tipo, preco, disponivel) VALUES (4, 'Prison Break', '1', '20,50', 'SIM');
+INSERT INTO produtos (id, nome, tipo, preco, disponivel) VALUES (5, 'E o Vento Levou', '2', '10,00', 'SIM');
+INSERT INTO produtos (id, nome, tipo, preco, disponivel) VALUES (6, 'Chernobyl', '3', '15,00', 'SIM');
---
+++ dadosClie.sql
@@ -0,0 +1,3 @@
+INSERT INTO clientes (id, nome, endereco, cpf, cidade_uf, idade, celular) VALUES (1, 'Taina', '''00000000000''', 'Rua Freitas', 'Coronel Freitas-SC', '23', '49/00000-0000');
+INSERT INTO clientes (id, nome, endereco, cpf, cidade_uf, idade, celular) VALUES (1, 'Julia', '''00000000000''', 'Rua Chapeco', 'Chapecó-SC', '23', '49/99999-0000');
+
